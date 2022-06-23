002b5ae2940dd740414c29376536808ddbd3d6f7
Josh Snyder <joshua.t.snyder@gmail.com>
2022-06-22 20:39:43 -0500

    doing some practice

--- arraysloopsExample.js
+++ arraysloopsExample.js
@@ -1,2 +1,15 @@
 console.log('Arrays example');
 let bagel = ['bacon', 'lettuce', 'tomato'];
+console.log('Loops example');
+
+let index = 0
+const max = 10
+
+//while Loop
+while (index < max) {
+    console.log('in while loop. index', index);
+    index++;
+}//end while
+console.log('index after while loop:', index);
+
+//for loop
