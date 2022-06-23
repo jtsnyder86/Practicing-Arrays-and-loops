0d40327c5ec757e30e086cc2354ccf02c05f660e
Josh Snyder <joshua.t.snyder@gmail.com>
2022-06-22 20:58:53 -0500

    did some more examples

--- arraysloopsExample.js
+++ arraysloopsExample.js
@@ -13,3 +13,34 @@ while (index < max) {
 console.log('index after while loop:', index);
 
 //for loop
+for (let i = 0; i < max; i++) {
+    console.log('in for loop. i:', i);
+    
+}//end for
+//console.log('i after for loop. i:', i)
+//doesn't work because 'i' only exists in 'for' code
+
+let hand = ['Q', '3', 'J', '9', '6', 'K', 'A'];
+
+for (let i = 0; i < hand.length; i++) {
+    console.log('for loop', hand [i]);
+    if(hand[i] === '9'){
+        console.log('match!!!');
+    }
+}//end for
+
+//for in loop
+for( i in hand){
+    console.log('for in loop', hand[i]);
+    if(hand[i] === '9'){
+        console.log('match!!!!');
+    }//end match
+}//end for in
+
+//for of
+for(card of hand){
+    console.log('for of loop', card);
+    if(card === '9'){
+        console.log('match!!!!!');
+    }//end match
+}//end for of
